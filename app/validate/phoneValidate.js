ad066d9e23e5aa709167ea83bce2d070b0ec6166
Thiago Soares <tsoaresaz@gmail.com>
2024-07-03 20:18:58 -0300

    realizado ajustes no Regex para celular e número residêncial

--- app/validate/phoneValidate.js
+++ app/validate/phoneValidate.js
@@ -2,7 +2,7 @@
  * @author Thiago Soares
  */
 
-const regex = /^(?:\(?\d{2}\)?\s?)\d{4,5}-\d{4}$/;
+const regex = /^(?:\(?\d{2}\)?\s?)\d{4,5}-\d{4,5}$/;
 
 export const ValidatePhone = phone => {
     console.log('phone validate => ', regex.test(phone));
